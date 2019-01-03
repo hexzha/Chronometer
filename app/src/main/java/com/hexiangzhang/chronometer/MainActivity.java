c0d314ee9d3338a28dd69a8cfdac8f19bbd60e2f
Hexiang Zhang <sam10.zhang@gmail.com>
2019-01-03 16:10:31 -0600

    add buttons and some logic

--- app/src/main/java/com/hexiangzhang/chronometer/MainActivity.java
+++ app/src/main/java/com/hexiangzhang/chronometer/MainActivity.java
@@ -2,12 +2,36 @@ package com.hexiangzhang.chronometer;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Chronometer;
 
 public class MainActivity extends AppCompatActivity {
+    private Chronometer chronometer;
+    private boolean running;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        chronometer = findViewById(R.id.chronometer);
+    }
+
+    public void startChronometer(View v) {
+        if(!running){
+            chronometer.start();
+            running = true;
+        }
+    }
+
+    public void pauseChronometer(View v) {
+        if(running){
+            chronometer.stop();
+            running = false;
+        }
+    }
+
+    public void resetChronometer(View v) {
+
     }
 }
