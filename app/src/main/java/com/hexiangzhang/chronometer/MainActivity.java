b7cf45f53538757151d4a3c7d639937011fbeae9
Hexiang Zhang <sam10.zhang@gmail.com>
2019-01-03 16:44:47 -0600

    Add string.xml, Fix chronometer not pausing issue

--- app/src/main/java/com/hexiangzhang/chronometer/MainActivity.java
+++ app/src/main/java/com/hexiangzhang/chronometer/MainActivity.java
@@ -1,12 +1,15 @@
 package com.hexiangzhang.chronometer;
 
+import android.os.SystemClock;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Chronometer;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     private Chronometer chronometer;
+    private long pauseOffset;
     private boolean running;
 
     @Override
@@ -15,23 +18,40 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         chronometer = findViewById(R.id.chronometer);
+        chronometer.setFormat("Time: %s");
+        chronometer.setBase(SystemClock.elapsedRealtime());
+
+        //Resets to 00:00 after running for an hour
+        chronometer.setOnChronometerTickListener(new Chronometer.OnChronometerTickListener() {
+            @Override
+            public void onChronometerTick(Chronometer chronometer) {
+                if ((SystemClock.elapsedRealtime() - chronometer.getBase()) >= 3600000) {
+                    chronometer.setBase(SystemClock.elapsedRealtime());
+                    Toast.makeText(MainActivity.this, "Bing!", Toast.LENGTH_SHORT).show();
+                }
+            }
+
+        });
     }
 
     public void startChronometer(View v) {
-        if(!running){
+        if (!running) {
+            chronometer.setBase(SystemClock.elapsedRealtime() - pauseOffset);
             chronometer.start();
             running = true;
         }
     }
 
     public void pauseChronometer(View v) {
-        if(running){
+        if (running) {
             chronometer.stop();
+            pauseOffset = SystemClock.elapsedRealtime() - chronometer.getBase();
             running = false;
         }
     }
 
     public void resetChronometer(View v) {
-
+        chronometer.setBase(SystemClock.elapsedRealtime());
+        pauseOffset = 0;
     }
 }
